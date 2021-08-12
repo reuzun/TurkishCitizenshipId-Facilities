0e8bd81b27ab24faf3b48cc88b4fe61fe319a50c
reuzun <moruka12@yandex.com>
2021-08-12 22:44:16 +0300

    A demo application added!

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,42 @@
+package demo;
+
+import model.BUILDER_TYPE;
+import model.TurkishCitizenshipId;
+import model.TurkishCitizenshipIdBuilder;
+
+import java.util.List;
+
+/**
+ * @author reuzun
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        TurkishCitizenshipId IdFinder = new TurkishCitizenshipIdBuilder()
+                .setId("22734---798")
+                .build(BUILDER_TYPE.FINDER);
+
+        List<String> list = IdFinder.listAvailableIds();
+
+        System.out.println("Possible Id's Using 22-34112-9- : \n------------------");
+        int count = 1;
+        for (String s : list) System.out.printf("%-2d -> %-11s\n", (count++), s);
+
+        System.out.println("\n***************************\n");
+
+        TurkishCitizenshipId RandomIdCreator = new TurkishCitizenshipIdBuilder()
+                .build(BUILDER_TYPE.CREATOR);
+
+        String randomId = RandomIdCreator.create();
+        System.out.println("An id that is randomly created : " + randomId);
+
+        System.out.println("\n***************************\n");
+
+        TurkishCitizenshipId IdValidator = new TurkishCitizenshipIdBuilder()
+                .setId("22734112798")
+                .build(BUILDER_TYPE.VALIDATOR);
+
+        System.out.println("Is 22734112798 a valid Id for Turkish Citizenship? " + IdValidator.validate());
+    }
+
+}
