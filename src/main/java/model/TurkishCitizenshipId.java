b2f6c0c9b2f560a58abf91b6cfca171fdc463d19
reuzun <moruka12@yandex.com>
2021-08-13 09:21:15 +0300

    Javadoc added to abstract TurkishCitizenshipId class.

--- src/main/java/model/TurkishCitizenshipId.java
+++ src/main/java/model/TurkishCitizenshipId.java
@@ -10,15 +10,29 @@ import java.util.List;
 public abstract class TurkishCitizenshipId {
 
 
-
+    /**
+     * Validates the id.
+     * @param id TurkishCitizenship Id
+     * @throws WrongTypeObjectException if BUILD_TYPE is not correct
+     * */
     public boolean validate(String id) throws Exception {
         throw new WrongTypeObjectException("Create model.TurkishCitizenshipIdValidator instead of that object.");
     }
 
+    /**
+     * Creates randomId
+     * @throws WrongTypeObjectException if BUILD_TYPE is not correct
+     * */
     public String create() throws WrongTypeObjectException {
         throw new WrongTypeObjectException("Create model.TurkishCitizenshipIdCreator instead of that object.");
     }
 
+    /**
+     * List possible Id's. With trying all combinations of - values with numbers.
+     * @param id A string including "-" ex: 145--7156--
+     * @return validated combinations of Id's
+     * @throws WrongTypeObjectException if BUILD_TYPE is not correct
+     * */
     public List<String> listAvailableIds(String id) throws Exception {
         throw new WrongTypeObjectException("Create model.TurkishCitizenshipIdFinder instead of that object.");
     }
